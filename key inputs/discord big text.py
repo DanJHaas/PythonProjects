b679432cafa2f79eab0d18161dedcfa41f49383d
HattyH124 <blueeyedan07@gmail.com>
2020-07-14 01:02:39 -0400

    big text comprimise

---
+++ key inputs/discord big text.py
@@ -0,0 +1,25 @@
+from pynput import keyboard
+from pynput.keyboard import Key, Controller
+message = []
+yes = Controller()
+    
+def on_press(key):
+    letter = str(key).replace("'","")
+    if key == keyboard.Key.esc:
+        for i in range(len(message)):
+            yes.press(Key.backspace)
+            yes.release(Key.backspace)
+        for i in range(len(message)):
+            yes.type(message[i])
+        return False
+    if(key == Key.space):
+        message.append("    ")
+    if(key == Key.space or key == Key.enter or key == Key.shift):
+        print("")
+    else:
+        message.append(":regional_indicator_"+str(letter)+": ")
+        
+lis = keyboard.Listener(on_press=on_press)
+lis.start()
+lis.join()
+
