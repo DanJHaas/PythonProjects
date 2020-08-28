cd909b3b441e8435641d4e9ad167a3bf84364aa0
HattyH124 <blueeyedan07@gmail.com>
2020-08-27 21:17:39 -0400

    testing

--- testing out code/test.py
+++ testing out code/test.py
@@ -1,5 +1,23 @@
+import pyglet
+from pyglet.gl import gl
+import ratcave as rc
+
 test = ["hello","goodbye"]
 
 for i,j in enumerate(test):
     print(j)
 
+window = pyglet.window.Window()
+
+
+
+@window.event
+def on_draw():
+    window.clear()
+
+
+def update(dt):
+    pass
+pyglet.clock.schedule(update)
+
+pyglet.app.run()
