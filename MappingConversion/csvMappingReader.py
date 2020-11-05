c1d7d60b2f024d6d1af56a9b14f05ac7fa990dfa
HattyH124 <blueeyedan07@gmail.com>
2020-11-05 12:42:53 -0500

    yes

--- MappingConversion/csvMappingReader.py
+++ MappingConversion/csvMappingReader.py
@@ -3,40 +3,28 @@ import fileinput
 from pathlib import Path
 import time
 t0 = time.time()
-fields = {}
-methods = {}
-params = {}
+mappings = {}
+lineschanged = 0
 
 for path in Path('MappingConversion//Mappings').rglob('*.csv'):
     with open(path, newline='') as csvfile:
         inputfile = csv.reader(csvfile, delimiter=',', quotechar='|')
         for csvinput in inputfile:
-            if "fields" in path.name:
-                fields[str(csvinput[0])] = str(csvinput[1])
-            if "methods" in path.name:
-                methods[str(csvinput[0])] = str(csvinput[1])
-            if "params" in path.name:
-                params[str(csvinput[0])] = str(csvinput[1])
-
-
-
+            mappings[str(csvinput[0])] = str(csvinput[1])
 
 for path in Path('witchery').rglob('*.java'):
     for line in fileinput.input(path, inplace=True):
         line = line.rstrip()
         if not line:
             continue
-        for key, val in fields.items():
-            if key in line:
-                line = line.replace(key, val)
-        for key, val in methods.items():
-            if key in line:
-                line = line.replace(key, val)
-        for key, val in params.items():
-            if key in line:
-                line = line.replace(key, val)
+        if "func_" or "field_" or "p_" in line:
+            for key, val in mappings.items():
+                if key in line:
+                    line = line.replace(key, val)
+                    lineschanged = lineschanged+1
         print(line)
-
 
 t1 = time.time() - t0
 print(t1)
+print(len(mappings))
+print(lineschanged)
