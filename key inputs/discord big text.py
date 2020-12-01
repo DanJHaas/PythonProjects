bb81205a6db2c5e918b37f4512cbef67bbb59d3b
HattyH124 <blueeyedan07@gmail.com>
2020-12-01 14:45:03 -0500

    fixed and cleaned some code, added numbers!

--- key inputs/discord big text.py
+++ key inputs/discord big text.py
@@ -1,26 +1,45 @@
+from re import match
 from pynput import keyboard
 from pynput.keyboard import Key, Controller
+import re
+
+### setup for variables and structs
 message = []
 yes = Controller()
-def here():
-    return True
+regstr = r"[a-z]"
+regnum = r"[0-9]"
+numbers={"0":"zero","1":"one","2":"two","3":"three","4":"four","5":"five","6":"six","7":"seven","8":"eight","9":"nine"}
 
+### main keypress def
 def on_press(key):
+    ### cleanup the input coming in from [key]
     letter = str(key).replace("'","")
+
+    ### typing out the result stored in an array
     if key == keyboard.Key.esc:
+        ### delete the original message
         for i in range(len(message)):
             yes.press(Key.backspace)
             yes.release(Key.backspace)
+        ### retype the new one
         for i in range(len(message)):
             yes.type(message[i])
         return False
-    if(key == Key.space):
+
+    ### use regex to figure out keys, looks cleaner
+    if key == Key.space:
         message.append("    ")
-    if(key == Key.space or key == Key.enter or key == Key.shift):
-        print("")
-    else:
+    ### looking for letters
+    elif bool(re.match(regstr, letter)):
         message.append(":regional_indicator_"+str(letter)+": ")
-        
+    ### lloking for numbers
+    elif bool(re.match(regnum, letter)):
+        message.append(":{0}:".format(numbers.get(letter)))
+    ### print something when any other key is pressed
+    else:
+        print("no key")
+
+### keyboard listener stuff
 lis = keyboard.Listener(on_press=on_press)
 lis.start()
 lis.join()
