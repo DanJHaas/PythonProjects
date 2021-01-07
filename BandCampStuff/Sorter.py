73dad69c0a8c3c3dd378275fa5a006d5e405766b
HattyH124 <blueeyedan07@gmail.com>
2021-01-07 08:45:30 -0500

    hi

--- BandCampStuff/Sorter.py
+++ BandCampStuff/Sorter.py
@@ -1,15 +1,10 @@
 import requests
-r =  requests.get(""+"/sitemap.xml", stream=True)
-
+r =  requests.get("https://aarongrooves.bandcamp.com/album/avm-16-note-block-battle-ost-animation-vs-minecraft-shorts-ep-16", stream=True)
 
 sc = 0
-for link in r.iter_lines(decode_unicode=True):
-    if "loc" in link:
-        cont = str(link).replace("<",";").replace(">",";").split(";")
-        for en in cont:
-            if r"https" in en:
-                sitemap = requests.get(en, stream=True)
-                for i in sitemap.iter_lines(decode_unicode=True):
+def scan(url):
+    global sc
+    for i in url.iter_lines(decode_unicode=True):
                     ha = []
                     if r"https://t4.bcbits" in i:
                         ha = str(i).split(";")
@@ -20,7 +15,21 @@ for link in r.iter_lines(decode_unicode=True):
                             sc+=1
                             l=requests.get(f"{ha[k]};{ha[k+1]};{ha[k+2]};{ha[k+3]}")
                             if l.ok:
-                                print(f"{sc} :",l.url)
+                                #print(f"{sc} :",l.url)
                                 open(f'dl/{sc}.mp3', 'wb').write(l.content)
                             else:
                                 print(f"Error {l.status_code}")
+
+
+if "sitemap" not in r.url:
+    scan(r)
+else:
+    for link in r.iter_lines(decode_unicode=True):
+        if "loc" in link:
+            print("op1")
+            cont = str(link).replace("<",";").replace(">",";").split(";")
+            for en in cont:
+                if r"https" in en:
+                    sitemap = requests.get(en, stream=True)
+                    scan(sitemap)
+                    
